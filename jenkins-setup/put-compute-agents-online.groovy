3b4973f69cc538dcd16b0ede4c6b9f96a4fdfb7d
Jason Swails <jason.swails@gmail.com>
2020-08-30 21:00:58 -0400

    Make reconnect agent job more robust

--- jenkins-setup/put-compute-agents-online.groovy
+++ jenkins-setup/put-compute-agents-online.groovy
@@ -3,6 +3,24 @@
  */
 import jenkins.model.Jenkins
 
+// Mark them *not* temporarily offline
 Jenkins.instance.computers.each { computer ->
     computer.setTemporarilyOffline(false)
+}
+
+/* While they are no longer marked offline temporarily, they may be disconnected
+ * and require launching the ssh agent. Check that here.
+ */
+Jenkins.instance.computers.each { computer ->
+    if (computer.isOffline()) {
+        println("INFO: ${computer.nodeName} is offline. Attempting to launch agent")
+        if (computer.isLaunchSupported()) {
+            println('INFO: Attempting launch...')
+            computer.connect(false)
+        } else {
+            println('WARN: Launching agent is not supported!')
+        }
+    } else {
+        println("INFO: ${computer.nodeName} is back online!")
+    }
 }
