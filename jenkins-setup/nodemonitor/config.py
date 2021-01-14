175a6bc957b3311df43a41e2bfaee046acd758e7
Jason Swails <jason.swails@gmail.com>
2021-01-13 23:52:57 -0500

    Make sure we have a password

--- jenkins-setup/nodemonitor/config.py
+++ jenkins-setup/nodemonitor/config.py
@@ -101,6 +101,8 @@ class NodemonitorConfiguration:
             ssh_config_kwargs["private_key"] = decrypt(ssh_config_kwargs["private_key"], PASSWORD)
         if "password" in ssh_config_kwargs:
             ssh_config_kwargs["password"] = decrypt(ssh_config_kwargs["password"], PASSWORD)
+        else:
+            ssh_config_kwargs["password"] = None
         ssh_config = SSHConfig.create(**ssh_config_kwargs)
         kwargs = dict(
             idle_time_before_launch=int(parser["SCHEDULER"].get("idle_time_before_launch", 300)),
